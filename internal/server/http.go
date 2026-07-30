a4f8665d581be7152020d8fbf55a394ac19ea792
habo <3547577928@qq.com>
2026-07-30 11:18:55 +0800

    增加限流器

--- internal/server/http.go
+++ internal/server/http.go
@@ -4,9 +4,12 @@ import (
 	v1 "testdemo/api/todo/v1"
 	"testdemo/internal/conf"
 	"testdemo/internal/service"
+	"time"
 
+	"github.com/go-kratos/aegis/ratelimit/bbr"
 	"github.com/go-kratos/kratos/contrib/otel/v3/metrics"
 	"github.com/go-kratos/kratos/contrib/otel/v3/tracing"
+	"github.com/go-kratos/kratos/v3/middleware/ratelimit"
 	"github.com/go-kratos/kratos/v3/middleware/recovery"
 	"github.com/go-kratos/kratos/v3/middleware/validate"
 	"github.com/go-kratos/kratos/v3/transport/http"
@@ -18,12 +21,14 @@ import (
 
 // NewHTTPServer new an HTTP serve r.
 func NewHTTPServer(c *conf.Server, mp metric.MeterProvider, todo *service.TodoService, greeter *service.GreeterService, user *service.UserService) *http.Server {
+
 	var opts = []http.ServerOption{
 		http.Middleware(
 			recovery.Recovery(),
 			tracing.Server(),
 			Logmiddle(),      //日志中间件
 			AuthMiddleware(), //认证中间件
+			ratelimit.Server(ratelimit.WithLimiter(NewRateLimiter())), //限流中间件
 			metrics.Server(
 				metrics.WithSeconds(metricSeconds),
 				metrics.WithRequests(metricRequests),
@@ -56,4 +61,11 @@ func NewHTTPServer(c *conf.Server, mp metric.MeterProvider, todo *service.TodoSe
 		return nil
 	})
 	return srv
+}
+func NewRateLimiter() ratelimit.Limiter {
+	return bbr.NewLimiter(
+		bbr.WithWindow(5*time.Second),
+		bbr.WithBuckets(100),
+		bbr.WithCPUThreshold(80),
+	)
 }
